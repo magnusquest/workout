93f049789856b00459f22e3c44f618d4e5d2d464
magnusquest <thisismartinc@gmail.com>
2024-11-11 14:23:06 -0500

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+from langflow.load import run_flow_from_json
+TWEAKS = {
+  "TextInput-xzxNz": {
+    "input_value": "question"
+  },
+  "TextInput-8YRVz": {
+    "input_value": "profile"
+  },
+}
+
+result = run_flow_from_json(flow="Ask AI.json",
+                            input_value="message",
+                            session_id="", # provide a session id if you want to use session state
+                            fallback_to_env_vars=True, # False by default
+                            tweaks=TWEAKS)
