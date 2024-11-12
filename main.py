3179434f813018bf2aee636955ed90b777cec80a
magnusquest <thisismartinc@gmail.com>
2024-11-11 23:45:11 -0500

    working ask_ai

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-from langflow.load import run_flow_from_json
-TWEAKS = {
-  "TextInput-xzxNz": {
-    "input_value": "question"
-  },
-  "TextInput-8YRVz": {
-    "input_value": "profile"
-  },
-}
+from dotenv import load_dotenv
+load_dotenv()
 
-result = run_flow_from_json(flow="AskAI.json",
-                            input_value="message",
-                            session_id="", # provide a session id if you want to use session state
-                            fallback_to_env_vars=False, # False by default
-                            tweaks=TWEAKS)
+from langflow.load import run_flow_from_json
+
+def ask_ai(profile, question):
+  TWEAKS = {
+    "TextInput-xzxNz": {
+      "input_value": question
+    },
+    "TextInput-8YRVz": {
+      "input_value": profile
+    },
+  }
+  result = run_flow_from_json(flow="AskAI.json",
+                input_value="message",
+                session_id="", # provide a session id if you want to use session state
+                fallback_to_env_vars=False, # False by default
+                tweaks=TWEAKS)
+  resultOutput = result[0].outputs[0].results["text"].data["text"]
+  return resultOutput
+
+
+
+
+def main():
+  output = ask_ai("How many meals should I be eating daily and how many calories each?", "Male, 28, 5'11, 180lbs")
+  print(output)
